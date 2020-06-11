1dfd6554a77bab2ac220a16c22db74767861f2e5
Mayron Souza <mayrondrummer@gmail.com>
2020-06-11 19:06:50 -0300

    Estudos sobre Scaffold e Decoration

--- lib/main.dart
+++ lib/main.dart
@@ -20,13 +20,38 @@ class _HomeState extends State<Home> {
           'Calculadora de IMC',
         ),
         centerTitle: true,
-        backgroundColor: Colors.deepPurple,
+        backgroundColor: Colors.cyan[600],
         actions: <Widget>[
           IconButton(
             icon: Icon(
               Icons.refresh,
             ),
             onPressed: () => print('Fui clicado!!!'),
+          ),
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Icon(
+            Icons.person_outline,
+            size: 120.0,
+            color: Colors.cyan,
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: 'Peso (Kg)',
+              labelStyle: TextStyle(
+                color: Colors.cyan,
+              ),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.cyan,
+              fontSize: 25.0,
+            ),
           ),
         ],
       ),
